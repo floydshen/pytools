9c563a14b180b9b7a98c68a4232898803e734394
Floyd Shen <floyd.shen@yahoo.com>
2013-05-15 21:58:47 +0800

    init

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+        name='pytools',
+        version='0.1.0',
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=[
+            'fabric==1.6.0',
+        ]
+        )
+
