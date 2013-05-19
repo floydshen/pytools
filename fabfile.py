5781ed118766dd9ee578e3d8e6a0e77b22b07135
Floyd Shen <floyd.shen@yahoo.com>
2013-05-19 22:07:39 +0800

    ajkroute

--- fabfile.py
+++ fabfile.py
@@ -19,7 +19,15 @@ env.hosts = ['localhost']
 
 @task
 def ajk_vpn():
-    sudo("add route ppp0 192.168.1.63")
+    sudo("route delete -net 192.168.1.24 -interface ppp0")
+    sudo("route delete -net 192.168.1.61 -interface ppp0")
+    sudo("route delete -net 192.168.1.170 -interface ppp0")
+    sudo("route delete -net 192.168.1.171 -interface ppp0")
+
+    sudo("route add -net 192.168.1.24 -interface ppp0")
+    sudo("route add -net 192.168.1.61 -interface ppp0")
+    sudo("route add -net 192.168.1.170 -interface ppp0")
+    sudo("route add -net 192.168.1.171 -interface ppp0")
 
 @task
 def new_pyproject():
